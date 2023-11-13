c5dfdf37d92bf80332a518c47cc0b44f5be7dda8
narmkhali <150644475+narmkhali@users.noreply.github.com>
2023-11-13 04:25:52 +0000

    Add files via upload

---
+++ rfv.py
@@ -0,0 +1,6 @@
+a=int(input('how much money do you have? '))
+d=int(input('how old are you? '))
+if d>=18 and a>=1100:
+    print('suruculuk vesiqesini ala bilersiniz')
+else:
+    print('suruculuk vesiqesini ala bilmezsiniz')
